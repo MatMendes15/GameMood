26c9f88d87140d916328f911a326e31bda67f594
MatMendes15 <matmendes2004@gmail.com>
2026-04-10 11:17:40 -0300

    fix: add modelos usados nas variaveis locais

--- config.py
+++ config.py
@@ -9,8 +9,6 @@ LANCEDB_URI = str(DATA_DIR / "lancedb")
 RAW_DIR.mkdir(parents=True, exist_ok=True)
 Path(LANCEDB_URI).mkdir(parents=True, exist_ok=True)
 
-LLM_MODEL = "gemma3:12B"
-EMBEDDING_MODEL = "nomic-embed-text"
 
 CHUNK_SIZE = 1000
 CHUNK_OVERLAP = 200
@@ -20,5 +18,7 @@ TABLE_SCIENCE = "saude_mental"
 
 
 load_dotenv()
+LLM_MODEL = os.getenv("LLM_MODEL")
+EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL")
 IGDB_CLIENT_ID = os.getenv("IGDB_CLIENT_ID")
 IGDB_CLIENT_SECRET = os.getenv("IGDB_CLIENT_SECRET")
